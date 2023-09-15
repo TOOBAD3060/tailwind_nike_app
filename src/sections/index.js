4845028f39027729da7b36d232bf701f764ecd5b
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2023-09-15 04:10:44 +0100

    Hero_section

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffers from "./SpecialOffers";
+import Suscribe from "./Suscribe";
+import Services from "./Services";
+import Footer from "./Footer"
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Suscribe,
+    Footer
+}
