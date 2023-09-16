1c5846d978a582f8d0a04964d36384a0d6221256
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2023-09-16 09:25:13 +0100

    specialOfferSection

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
-const Button = ({label, iconURL}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, 
+borderColor}) => {
   return (
     
-    <button className="flex justify-center
+    <button className= {`flex justify-center
     items-center gap-2 px-7 py-4 border font-montserrat
-    text-lg heading-none bg-coral-red
-    rounded-full text-white">
+    text-lg heading-none
+    
+      ${backgroundColor 
+        ? `${backgroundColor} ${textColor} ${borderColor} `
+        : 'bg-coral-red  text-white border-coral-red' }
+    rounded-full
+    `} >
         {label}
-        <img src={iconURL} 
+        {iconURL && <img src={iconURL} 
         alt="arrow-right-icon"
-        className='ml-2 rounded-full w-5 h-5'  />
+        className='ml-2 rounded-full w-5 h-5'  />}
     </button>
   )
 }
