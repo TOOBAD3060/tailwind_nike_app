a7ebab428552de4f027bdd69e249499a4702bcc9
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2023-09-18 08:03:29 +0100

    now in Suscribe Section

--- src/sections/Suscribe.jsx
+++ src/sections/Suscribe.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Suscribe = () => {
   return (
-    <div>Suscribe</div>
+    <section
+    id='contact-us' className="max-container flex 
+    justify-between items-center max-lg:flex-col
+    gap-10 ">
+        <h3 className="text-4xl leading-[68px] 
+         font-palanquin font-bold">
+          Sign Up from
+          <span className='text-coral-red'> Updates </span>
+          & Newsletter
+        </h3>
+
+        <div className="">
+          
+        </div>
+    </section>
   )
 }
 
