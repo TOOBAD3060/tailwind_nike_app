1c7d6bfd221ee09c0ce3c3d7996c5e01121466e4
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2023-11-09 23:10:14 +0100

    removedHeroBorder

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
     <section id="home" className="w-full flex
     xl:flex-row flex-col justify-center min-h-screen
     gap-10 max-container
-     border-2 border-red-400 ">
+ ">
 
       <div className="relative
       xl:w-2/5 flex flex-col justify-center
